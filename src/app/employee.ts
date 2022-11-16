3e1ccb99ad3e5e5c8705102cd22cddee879116c3
Armon Antolin <74826437+JArmonTeach@users.noreply.github.com>
2022-11-16 17:37:38 -0500

    build: add employee.ts and service

---
+++ src/app/employee.ts
@@ -0,0 +1,12 @@
+export interface Employee {
+    id: number;
+    firstName: string;
+    lastName: string;
+    email: string;
+    phone: string;
+    department: string;
+    jobTitle: string;
+    location: string;
+    imageUrl: string;
+    employeeCode: string;
+}
