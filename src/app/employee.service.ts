b7c39fc146917c0f14dc831a5624a022b1160f04
Armon Antolin <74826437+JArmonTeach@users.noreply.github.com>
2022-11-26 20:26:07 -0500

    debug: add apiUrl to environment and service

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -2,13 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Employee } from './employee';
-import { environment } from 'src/environments/environment';
+import { environment } from 'src/environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
-  private apiServerUrl = environment.apiBaseurl;
+  private apiServerUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
 
